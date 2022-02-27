da0cf8e65de5adf95b0a0ef84407ea3b384d19dd
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2022-02-27 12:48:02 -0800

    Final check and testing.

--- PVOT/Controllers/HomeController.cs
+++ PVOT/Controllers/HomeController.cs
@@ -1,7 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace PVOT.Controllers
+namespace VOT.Controllers
 {
     public class HomeController : Controller
-        {} }
-	
+    {
+
+[HttpGet("/")]
+public ActionResult Index()
+    {
+        return View();
+    }
+
+    }
+}

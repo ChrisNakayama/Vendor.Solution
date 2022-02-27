debd6454a95522baabb503d3b3baf0324d878d33
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2022-02-27 08:10:44 -0800

    Added Vendor GetAll ClearALL Passed.

--- PVOT.Tests/ModelTests/VendorTests.cs
+++ PVOT.Tests/ModelTests/VendorTests.cs
@@ -6,8 +6,12 @@ using System;
 namespace PVOT.Tests
 {
   [TestClass]
-  public class VendorTests
+  public class VendorTests : IDisposable
   {
+     public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
     [TestMethod]
     public void VendorConstructor_TypeIsVendor_Vendor()
     {
@@ -33,6 +37,22 @@ namespace PVOT.Tests
     {
       Vendor newVendor = new Vendor("Tanner H", "Coffee Shop");
       Assert.AreEqual("Coffee Shop", newVendor.Description);
+    }
+    [TestMethod]
+    public void SetDescription_ReturnsUpdatedDescription_String()
+    {
+      Vendor newVendor = new Vendor("Tanner H", "Coffee Shop");
+      newVendor.Description = "Record Store and Coffee Shop";
+      Assert.AreEqual("Record Store and Coffee Shop", newVendor.Description);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllVendors_VendorList()
+    {
+      Vendor newVendor = new Vendor("Tanner H", "Coffee Shop.");
+      Vendor anotherVendor = new Vendor("Jamie P", "Florest");
+      List<Vendor> vendorList = new List<Vendor> {newVendor, anotherVendor};
+      CollectionAssert.AreEqual(vendorList, Vendor.GetAll());
     }
   }
 }

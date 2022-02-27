709b6fc50b95c6271f09e721f6a2f997361e24c7
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2022-02-27 08:48:57 -0800

    Added Order properties and passed test.

--- PVOT.Tests/ModelTests/OrderTests.cs
+++ PVOT.Tests/ModelTests/OrderTests.cs
@@ -19,5 +19,30 @@ namespace PVOT.Tests
       Order newOrder = new Order("Order 1", "10 bread, 50 pastries", 45.00, "2022-02-25");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+    [TestMethod]
+    public void GetTitle_ReturnTitle_String()
+    {
+      Order newOrder = new Order("Order 1", "10 bread, 50 pastries", 45.00, "2022-02-25");
+      Assert.AreEqual("Order 1", newOrder.Title);
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnDescription_String()
+    {
+      Order newOrder = new Order("Order 1", "10 bread, 50 pastries", 45.00, "2022-02-25");
+      Assert.AreEqual("10 bread, 50 pastries", newOrder.Description);
+    }
+    [TestMethod]
+    public void GetPrice_ReturnPrice_Double()
+    {
+      Order newOrder = new Order("Order 1", "10 bread, 50 pastries", 45.00, "2022-02-25");
+      Assert.AreEqual(45.00, newOrder.Price);
+    }
+    [TestMethod]
+    public void GetDate_ReturnDate_String()
+    {
+      Order newOrder = new Order("Order 1", "10 bread, 50 pastries", 45.00, "2022-02-25");
+      Assert.AreEqual("2022-02-25", newOrder.Date);
+    }
   }
   }

e2abb71d66302d27bc667770808aa512dc7c9fbc
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2022-02-27 10:46:51 -0800

    Work on Views side and Order.

--- PVOT/Controllers/OrderController.cs
+++ PVOT/Controllers/OrderController.cs
@@ -7,6 +7,13 @@ namespace PVOT.Controllers
 {
   public class OrderController : Controller
   {
+    [HttpGet("/vendor/{Id}/order")]
+    public ActionResult Index(int vendorId)
+    {
+      Vendor currentVendor = Vendor.Find(vendorId);
+      List<Order> vendorOrders = currentVendor.Orders;
+      return View(vendorOrders);
+    }
 
   }
 }

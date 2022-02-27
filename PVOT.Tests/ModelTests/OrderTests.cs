27b2ee9f296be6af00cf61eeba26d425aff9a395
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2022-02-27 08:33:23 -0800

    Added Order Constructor and set Properties of Order.

--- PVOT.Tests/ModelTests/OrderTests.cs
+++ PVOT.Tests/ModelTests/OrderTests.cs
@@ -6,7 +6,18 @@ using System;
 namespace PVOT.Tests
 {
   [TestClass]
-  public class OrderTests
+  public class OrderTests: IDisposable
   {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
 
+    [TestMethod]
+    public void OrderConstructor_CheckTypeOrder_Order()
+    {
+      Order newOrder = new Order("Order 1", "10 bread, 50 pastries", 45.00, "2022-02-25");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
   }

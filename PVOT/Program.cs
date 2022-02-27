c594e59c2d054991ca40fe242d0fa65bc33e62f7
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2022-02-27 11:50:46 -0800

    Entry point in program.cs.

--- PVOT/Program.cs
+++ PVOT/Program.cs
@@ -1,8 +1,20 @@
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
 
-namespace PVOT
+namespace VOT
 {
   public class Program
-  {}}
-	
+  {
+    public static void Main(string[] args)
+    {
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
+    }
+  }
+}
